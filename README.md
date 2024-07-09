ab5ca4b9a39efb6de5735553dde209c86b4f06fe
Selim <mesic.selim@outlook.com>
2024-07-09 08:53:42 +0200

    Changed GUI lib to GTK

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Araz-Browser
+
+# Dev install
+- sudo apt update
+- sudo apt install libgtk-3-dev
+## To compile use:
+- gcc main.c -o araz_browser `pkg-config --cflags --libs gtk+-3.0`
--- main.c
+++ main.c
@@ -1,46 +1,31 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <gtk/gtk.h>
 
-#define NK_IMPLEMENTATION
-#include "libs/nuklear.h"
+// Called when the application is activated
+static void activate(GtkApplication* app, gpointer user_data) {
+    GtkWidget *window;
 
-int main()
-{
-	// init gui state
-	enum {EASY, HARD};
-	static int op = EASY;
-	static float value = 0.6f;
-	static int i =  20;
-	
-    struct nk_user_font font;
-	float wd = 5.5;
-    font.userdata.ptr = &font;
-    font.height = 10.0;
-    font.width = &wd;
+    // Create a new window
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello, GTK!");
+    gtk_window_set_default_size(GTK_WINDOW(window), 1200, 800);
 
-	struct nk_context ctx;
-	nk_init_fixed(&ctx, calloc(1, 1000), 1000, &font);
-	if (nk_begin(&ctx, "Show", nk_rect(50, 50, 220, 220),
-		NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_CLOSABLE)) {
-		// fixed widget pixel width
-		nk_layout_row_static(&ctx, 30, 80, 1);
-		if (nk_button_label(&ctx, "button")) {
-			// event handling
-		}
-		// fixed widget window ratio width
-		nk_layout_row_dynamic(&ctx, 30, 2);
-		if (nk_option_label(&ctx, "easy", op == EASY)) op = EASY;
-		if (nk_option_label(&ctx, "hard", op == HARD)) op = HARD;
-		// custom widget pixel width
-		nk_layout_row_begin(&ctx, NK_STATIC, 30, 2);
-		{
-			nk_layout_row_push(&ctx, 50);
-			nk_label(&ctx, "Volume:", NK_TEXT_LEFT);
-			nk_layout_row_push(&ctx, 110);
-			nk_slider_float(&ctx, 0, &value, 1.0f, 0.1f);
-		}
-		nk_layout_row_end(&ctx);
-	}
-	nk_end(&ctx);
-    return 0;
+    // Show the window
+    gtk_widget_show_all(window);
+}
+
+int main(int argc, char **argv) {
+    GtkApplication *app;
+    int status;
+
+    // Create a new GTK application
+    app = gtk_application_new("org.example.gtkdemo", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+
+    // Run the application
+    status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    // Cleanup
+    g_object_unref(app);
+
+    return status;
 }
